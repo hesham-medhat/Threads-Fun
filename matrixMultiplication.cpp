f7c1915da7783b574a26c26f996db48f74af6769
Hesham Medhat <heshammedhat5@gmail.com>
2018-10-25 21:34:32 +0200

    Implement multithreaded matrixMultiplication

--- matrixMultiplication.cpp
+++ matrixMultiplication.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <time.h>
 
 std::ifstream ifs;// Input file stream at <inputFileName>.
 std::ofstream ofs;// Output file stream at <outputFileName>.
@@ -13,11 +14,29 @@ const std::string logFileName = "log.log";
 int r1, c1, r2, c2;// Rows and columns of matrices 1 and 2.
 std::vector<std::vector<int>> matrix1, matrix2;
 
+// Represents the stack node for the threads in procedure 1
+struct rcRunnerParam {
+    int r;
+    int c;
+    int result;
+};
+
+// Represents the stack node for the threads in procedure 2
+struct rRunnerParam {
+    int r;
+    std::vector<int> result;
+};
 
 void openFileStreams() {
     ifs.open(inputFileName);
     ofs.open(outputFileName);
     lfs.open(logFileName);
+}
+
+void closeFileStreams() {
+    ifs.close();
+    ofs.close();
+    lfs.close();
 }
 
 // Logs intended logMessage to log file <logFileName> or to stdout on failure.
@@ -39,6 +58,33 @@ void readMatrix(int rows, int columns, std::vector<std::vector<int>> &matrix) {
     }
 }
 
+void* calculateElement(void* rcParam) {
+    int element = 0;
+    int r = ((rcRunnerParam*) rcParam)->r;
+    int c = ((rcRunnerParam*) rcParam)->c;
+    int i1 = 0, i2 = 0;
+    while (i1 < c1) {
+        element += matrix1[r][i1++] * matrix2[i2++][c];
+    }
+    ((rcRunnerParam*) rcParam)->result = element;
+    return nullptr;
+}
+
+void* calculateRow(void* rParam) {
+    int element;
+    int row = ((rRunnerParam*) rParam)->r;
+    std::vector<int> result = ((rRunnerParam*) rParam)->result;
+    int i1, i2;
+    for (int c = 0; c < c2; c++) {
+        i1 = i2 = element = 0;
+        while (i1 < c1) {
+            element += matrix1[row][i1++] * matrix2[i2++][c];
+        }
+        result.push_back(element);
+    }
+    return nullptr;
+}
+
 int main() {
     openFileStreams();
     if (ifs.is_open()) {
@@ -55,20 +101,71 @@ int main() {
 
         if (c1 != r2) {
             log("Matrices cannot be multiplied due to violation of dimensions rule.");
+            closeFileStreams();
             return 1;
         } else {
-            /* TODO: Matrix multiplication procedure #1:
+            /* Matrix multiplication procedure #1:
              * The computation of each element of the output matrix happens in a thread.
              */
+            time_t startTime1, startTime2, endTime1, endTime2;
+            time(&startTime1);
+            int outputMatrix1[r1][c2];
+            // Prepare and run threads
+            pthread_t threads1[r1][c2];
+            rcRunnerParam params1[r1][c2];
+            for (int r = 0; r < r1; r++) {
+                for (int c = 0; c < c2; c++) {
+                    params1[r][c].r = r;
+                    params1[r][c].c = c;
+                    pthread_create(&threads1[r][c], nullptr, calculateElement, &params1[r][c]);
+                }
+            }
+            // Collect and output results
+            for (int r = 0; r < r1; r++) {
+                for (int c = 0; c < c2; c++) {
+                    pthread_join(threads1[r][c], nullptr);
+                    outputMatrix1[r][c] = params1[r][c].result;
+                    ofs << outputMatrix1[r][c] << " ";
+                }
+                ofs << std::endl;
+            }
+            time(&endTime1);
+            ofs << difftime(startTime1, endTime1) << std::endl;
+            log("Finished procedure 1 successfully.");
 
-
-            /* TODO: Matrix multiplication procedure #2:
+            /* Matrix multiplication procedure #2:
              * The computation of each row of the output matrix happens in a thread.
              */
+            time(&startTime2);
+            int outputMatrix2[r1][c2];
+            // Prepare and run threads
+            pthread_t threads2[r1];
+            rRunnerParam params2[r1];
+            for (int r = 0; r < r1; r++) {
+                params2[r].r = r;
+                params2[r].result.reserve(c2);
+                pthread_create(&threads2[r], nullptr, calculateRow, &params2[r]);
+            }
+            // Collect and output results
+            for (int r = 0; r < r1; r++) {
+                pthread_join(threads2[r], nullptr);
+                for (int i = 0; i < c2; i++) {
+                    outputMatrix2[r][i] = params2[r].result[i];
+                    ofs << outputMatrix1[r][i] << " ";
+                }
+                ofs << std::endl;
+            }
+            time(&endTime2);
+            ofs << difftime(startTime2, endTime2) << std::endl;
+            log("Finished procedure 2 successfully.");
+
+            log("Successful termination.");
+            closeFileStreams();
         }
 
     } else {
         log("Input file read failed.");
+        closeFileStreams();
         return 1;
     }
     return 0;
