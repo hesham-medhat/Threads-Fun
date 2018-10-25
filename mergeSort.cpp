d3ce229fd2ecd6c58df62416322e614569ffd4bf
Hesham Medhat <heshammedhat5@gmail.com>
2018-10-25 18:20:39 +0200

    Implement merge sort algorithm

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <pthread.h>
 
 std::ifstream ifs;// Input file stream at <inputFileName>.
 std::ofstream ofs;// Output file stream at <outputFileName>.
@@ -11,6 +12,7 @@ const std::string logFileName = "log.log";
 
 int n;// Size of the array
 int* arr;
+int* aux;
 
 void openFileStreams() {
     ifs.open(inputFileName);
@@ -30,21 +32,65 @@ void log(std::string const &logMessage) {
 void readInput() {
     log("Reading input.");
     ifs >> n;
-    int reserve[n];
-    arr = reserve;
+    int arrReserve[n];
+    int auxReserve[n];
+    arr = arrReserve;
+    aux = auxReserve;
     for(int i = 0; i < n; i++) {
         ifs >> arr[i];
     }
     log("Input read successfully.");
 }
 
+void merge(int start, int mid, int end) {
+    // Copy left portion
+    for(int i = start; i <= mid; i++) {
+        aux[i] = arr[i];
+    }
+    // Copy right portion
+    for(int i = mid + 1; i <= end; i++) {
+        aux[i] = arr[i];
+    }
+    // Merge
+    int l = start, r = mid + 1;
+    int index = start;
+    while(l <= mid && r <= end) {
+        if (aux[l] < aux[r]) {
+            arr[index] = aux[l++];
+        } else {
+            arr[index] = aux[r++];
+        }
+        index++;
+    }
+    // Put remaining
+    while(l <= mid) {
+        arr[index++] = aux[l++];
+    }
+    while(r <= end) {
+        arr[index++] = aux[r++];
+    }
+}
+
+// Merge sorts arr
+void mergeSort(int start, int end) {
+    int mid = (start + end) / 2;
+    mergeSort(start, mid);
+    mergeSort(mid + 1, end);
+    merge(start, mid, end);
+}
+
+void printArray() {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << std::endl;
+    }
+}
+
 int main() {
     openFileStreams();
     if (ifs.is_open()) {
         readInput();
-
-        // TODO: Merge sort the array.
-
+        mergeSort(0, n - 1);
+        printArray();
     } else {
         log("Input file read failed.");
         return 1;
